08a85e4107e6d9778e21df130e03a20dd4ed3e33
Jeff Putz <jeff@popw.com>
2020-10-18 18:39:06 -0400

    make song grid a component, tweak context menu, format tables

--- src/MLocker.WebApp/Services/SongContextStateService.cs
+++ src/MLocker.WebApp/Services/SongContextStateService.cs
@@ -5,16 +5,24 @@ namespace MLocker.WebApp.Services
 	public interface ISongContextStateService
 	{
 		Song Song { get; set; }
+		string ContextMenuID { get; set; }
 	}
 
 	public class SongContextStateService : ISongContextStateService
 	{
 		private static Song _song;
+		private static string _contextMenuID;
 
 		public Song Song
 		{
 			get => _song;
 			set => _song = value;
+		}
+
+		public string ContextMenuID
+		{
+			get => _contextMenuID;
+			set => _contextMenuID = value;
 		}
 	}
 }
