6ebc9482980f9a8f494b9bbb935c188c1fa482be
Jeff Putz <jeff@popw.com>
2020-10-27 23:58:33 -0400

    Go to album context menu

--- src/MLocker.WebApp/Services/SongContextStateService.cs
+++ src/MLocker.WebApp/Services/SongContextStateService.cs
@@ -7,6 +7,7 @@ namespace MLocker.WebApp.Services
 		Song Song { get; set; }
 		string ContextMenuID { get; set; }
 		Playlist LastUsedPlaylist { get; set; }
+		bool IsToAlbumLoad { get; set; }
 	}
 
 	public class SongContextStateService : ISongContextStateService
@@ -14,6 +15,7 @@ namespace MLocker.WebApp.Services
 		private static Song _song;
 		private static string _contextMenuID;
 		private static Playlist _lastUsedPlaylist;
+		private static bool _isToAlbumLoad;
 
 		public Song Song
 		{
@@ -31,6 +33,12 @@ namespace MLocker.WebApp.Services
 		{
 			get => _lastUsedPlaylist;
 			set => _lastUsedPlaylist = value;
+		}
+
+		public bool IsToAlbumLoad
+		{
+			get => _isToAlbumLoad;
+			set => _isToAlbumLoad = value;
 		}
 	}
 }
