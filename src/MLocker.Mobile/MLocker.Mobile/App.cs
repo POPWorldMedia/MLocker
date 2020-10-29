3b1e633e531e005149e97ca37d188c1b9b297e48
Jeff Putz <jeff@popw.com>
2020-10-29 19:37:26 -0400

    initial attempt at xamarin MobileBlazorBindings

---
+++ src/MLocker.Mobile/MLocker.Mobile/App.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.MobileBlazorBindings;
+using Microsoft.Extensions.Hosting;
+using MLocker.Mobile.Repositories;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace MLocker.Mobile
+{
+    public class App : Application
+    {
+        public App()
+        {
+            var host = MobileBlazorBindingsHost.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    // Register app-specific services
+                    //services.AddSingleton<AppState>();
+                    services.AddSingleton<ISongRepository, SongRepository>();
+                })
+                .Build();
+
+            MainPage = new ContentPage();
+            host.AddComponent<HelloWorld>(parent: MainPage);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ src/MLocker.Mobile/MLocker.Mobile/Repositories/SongRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+using MLocker.Core.Models;
+
+namespace MLocker.Mobile.Repositories
+{
+	public interface ISongRepository
+	{
+		Task<List<Song>> GetAllSongs();
+	}
+
+	public class SongRepository : ISongRepository
+	{
+		private readonly HttpClient _httpClient;
+
+		public SongRepository()
+		{
+			var handler = new HttpClientHandler();
+			handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
+			{
+				if (cert.Issuer.Equals("CN=localhost"))
+					return true;
+				return errors == System.Net.Security.SslPolicyErrors.None;
+			};
+			_httpClient = new HttpClient(handler) {BaseAddress = new Uri("https://10.0.2.2:5001") };
+		}
+
+		public async Task<List<Song>> GetAllSongs()
+		{
+			var apiKey = "123";
+			_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(apiKey);
+			var test = await _httpClient.GetAsync(ApiPaths.GetAllSongs);
+			var response = await _httpClient.GetStringAsync(ApiPaths.GetAllSongs);
+			var allSongs = JsonSerializer.Deserialize<IEnumerable<Song>>(response, new JsonSerializerOptions(JsonSerializerDefaults.Web));
+			return allSongs.ToList();
+		}
+    }
+}
