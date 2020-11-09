ca8a25418814c626fea8460c88f625364ed1b731
Jeff Putz <jeff@popw.com>
2020-11-08 21:45:47 -0500

    add the PWA plumbing

--- src/MLocker.WebApp/wwwroot/sw.js
+++ src/MLocker.WebApp/wwwroot/sw.js
@@ -1,8 +1,21 @@
 ﻿var CACHE_NAME = 'music-image-cache';
 
+const cacheNamePrefix = 'offline-cache-';
+const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
+const offlineAssetsInclude = [/\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/];
+const offlineAssetsExclude = [/^service-worker\.js$/];
+
+self.importScripts('./service-worker-assets.js');
+
 self.addEventListener('install', event => {
-		self.skipWaiting();
-	});
+	self.skipWaiting();
+	// Fetch and cache all matching items from the assets manifest
+	const assetsRequests = self.assetsManifest.assets
+		.filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
+		.filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
+		.map(asset => new Request(asset.url, { integrity: asset.hash }));
+	await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
+});
 
 self.addEventListener('fetch', event => {
 	var cacheControl = event.request.headers.get('cache-control');
