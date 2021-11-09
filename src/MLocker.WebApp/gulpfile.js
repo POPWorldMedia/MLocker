159330a4bfc78b8ff7002e6206160bc427e68a7c
Jeff Putz <jeff@popw.com>
2021-11-08 23:11:53 -0500

    make bootstrap and popper be local to the solution instead of CDN links

---
+++ src/MLocker.WebApp/gulpfile.js
@@ -0,0 +1,17 @@
+﻿/// <binding AfterBuild="default" />
+
+var gulp = require("gulp"),
+    merge = require("merge-stream");
+
+var nodeRoot = "./node_modules/";
+var targetPath = "./wwwroot/lib/";
+
+gulp.task("copies", function () {
+    var streams = [
+        gulp.src(nodeRoot + "bootstrap/dist/**/*").pipe(gulp.dest(targetPath + "/bootstrap/dist")),
+        gulp.src(nodeRoot + "@popperjs/core/dist/umd/**/*").pipe(gulp.dest(targetPath + "/popperjs/dist"))
+    ];
+    return merge(streams);
+});
+
+gulp.task("default", gulp.series("copies"));
